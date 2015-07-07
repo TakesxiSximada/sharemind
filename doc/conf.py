5f6363fc31e4757931f57886d99c146625a991db
TakesxiSximada <takesxi.sximada@gmail.com>
2015-07-08 03:49:58 +0900

    add strike role

--- doc/conf.py
+++ doc/conf.py
@@ -276,3 +276,7 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+
+def setup(app):
+    app.add_stylesheet('custom.css')
