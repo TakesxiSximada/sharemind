d01a9c6e14059175fefbd6a06fe722d9b526db28
TakesxiSximada <takesxi.sximada@gmail.com>
2015-08-14 22:51:43 +0900

    config webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// -*- coding: utf-8 -*-
+
+module.exports = {
+    entry: {
+        my_mind: __dirname + '/lib/my-mind/my-mind.js',
+    },
+    output: {
+        path: __dirname + '/www',
+        filename: 'bundle.js',
+    },
+}
